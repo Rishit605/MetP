9fc205d06530e7d46d3c47d75df7e6907f232b7a
Rishit605 <rishit.sisodia2001@gmail.com>
2024-04-25 16:04:49 +0530

    Phase 3 - Cont...
    Custom model implementation is complete. To be Tested.

--- src/preprocessing/training.py
+++ src/preprocessing/training.py
@@ -1,67 +1,29 @@
 import pandas as pd
 import numpy as np
 
-from data_pipeline import pre_process
-from sklearn.preprocessing import MinMaxScaler
+from tensorflow import keras
+from tensorflow.keras.callbacks import ModelCheckpoint
 
-def scaler():
-  scale = MinMaxScaler(feature_range=(0,1))
-
-  _X = window_make()
+from model import SimpleLSTMModel
+from data_pipeline import pre_process, 
 
 
-def window_make(df):
-  df_as_np = df.to_numpy()
+epochs = 5
+batch_size = 16
 
+n_features = len(pre_process().columns)
 
-  X=[]
-  y=[]
+model = SimpleLSTMModel(n_features=n_features, n_target_variables=n_features)
 
-  for i in range(len(df_as_np)-window_size):
-    row = [a for a in df_as_np[i : i + window_size]]
-    X.append(row)
+# Define callback to save the best model based on validation loss
+filepath = r'C:\Projs\COde\Meteo\MetP\src\model\best_weather_model.keras'  # Path to save the best model
+checkpoint = ModelCheckpoint(filepath, monitor='val_loss', save_best_only=True, mode='min')
 
-    label = [df_as_np[i + window_size][0],
-             df_as_np[i + window_size][1],
-             df_as_np[i + window_size][2],
-             df_as_np[i + window_size][3],
-             df_as_np[i + window_size][4]]
-    y.append(label)
+losses = keras.losses.MeanSquaredError()
+metric = keras.metrics.RootMeanSquaredError()
+opt = keras.optimizers.Adam(learning_rate=1e-4)
 
-  return np.array(X), np.array(y)
+model.compile(loss=losses, metrics=[metric], optimizer=opt)
+model.fit(X1_Train, y1_Train, epochs=epochs, batch_size=batch_size, vlaidation_data=(X1_Val, y1_Val), callbacks=[checkpoint])
 
-
-def ret_split_data(data: pd.DataFrame):
-    X1, y1 = window_make(data)
-
-    train_split = int(len(X1) * (70 / 100))
-
-    if len(data) < 10000: 
-        valid_split = int(len(X1) - train_split)
-
-        # Splitting the Dataset
-        X1_train, y1_train = X1[:train_split], y1[:train_split]
-        X1_val, y1_val = X1[train_split:valid_split], y1[train_split:valid_split]
-        return X1_train, y1_train, X1_val, y1_val
-
-
-    else:
-        valid_split = int(len(X1) - train_split)
-        test_split = int(valid_split - (50 / 100))
-
-        # Splitting the Dataset
-        X1_train, y1_train = X1[:train_split], y1[:train_split]
-        X1_val, y1_val = X1[train_split:valid_split], y1[train_split:valid_split]
-        X1_test, y1_test = X1[valid_split:test_split], y1[valid_split:test_split]
-        return X1_train, y1_train, X1_val, y1_val, X1_test, y1_test
-    
-
-res = ret_split_data(pre_process())
-
-if isinstance(res, tuple):
-  if len(result) == 4:
-    X_Train, y_Train, X_Test, y_Test = ret_split_data(pre_process())
-  elif len(result) == 6:
-    X_Train, y_Train, X_Val, y_Val, X_Test, y_Test = ret_split_data(pre_process())
-
-
+### Data Calling is LEFT
