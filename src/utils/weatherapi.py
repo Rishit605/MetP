15187321cfb4323cf32df41e23ef1edd4694b4fd
Rishit605 <rishit.sisodia2001@gmail.com>
2024-05-14 12:04:50 +0530

    Update weatherapi.py

--- src/utils/weatherapi.py
+++ src/utils/weatherapi.py
@@ -9,10 +9,10 @@ import pandas as pd
 url_WB = 'https://api.weatherbit.io/v2.0/history/hourly?lat=23.25&lon=77.25&country=India&start_date=2024-01-01&end_date=2024-04-01&key=fcac888d541149bcabe62e93c304cd29'
 # url_OWM = 'https://api.openweathermap.org/data/2.5/forecast?lat=23.2599&lon=77.4126&appid=c9d4c0f1fb50e0f4786f71ad1446fdc9&units=metric'
 
-bpl_url_OWM = 'https://api.openweathermap.org/data/2.5/onecall?appid=2710adb6f6e634dedf12c34ca5bb82e1&units=metric&lang=&lat=23.2599&lon=77.4126&q=&unit_c=%C2%B0C&unit_f=%C2%B0FGETundefined'
-bgl_url_OWM = 'https://api.openweathermap.org/data/2.5/onecall?appid=2710adb6f6e634dedf12c34ca5bb82e1&units=metric&lang=&lat=12.9716&lon=77.5946&q=&unit_c=%C2%B0C&unit_f=%C2%B0FGETundefined'
-gn__url_OWM = 'https://api.openweathermap.org/data/2.5/onecall?appid=2710adb6f6e634dedf12c34ca5bb82e1&units=metric&lang=&lat=23.2156&lon=72.6369&q=&unit_c=%C2%B0C&unit_f=%C2%B0FGETundefined'
-srin__url_OWM = 'https://api.openweathermap.org/data/2.5/onecall?appid=2710adb6f6e634dedf12c34ca5bb82e1&units=metric&lang=&lat=34.0837&lon=74.7973&q=&unit_c=%C2%B0C&unit_f=%C2%B0FGETundefined'
+bpl_url_OWM = 'https://api.openweathermap.org/data/2.5/forecast?lat=23.2599&lon=77.4126&appid=c9d4c0f1fb50e0f4786f71ad1446fdc9&units=metric'
+bgl_url_OWM = 'https://api.openweathermap.org/data/2.5/forecast?lat=12.9716&lon=77.5946&appid=c9d4c0f1fb50e0f4786f71ad1446fdc9&units=metric'
+gn__url_OWM = 'https://api.openweathermap.org/data/2.5/forecast?lat=23.2156&lon=72.6369&appid=c9d4c0f1fb50e0f4786f71ad1446fdc9&units=metric'
+srin__url_OWM = 'https://api.openweathermap.org/data/2.5/forecast?lat=34.0837&lon=74.7973&appid=c9d4c0f1fb50e0f4786f71ad1446fdc9&units=metric'
 
 cities_url = {
     'Bhopal_URL': bpl_url_OWM,
@@ -66,7 +66,7 @@ def api_call_Open_weather_Map(url) -> pd.DataFrame:
         # Parse the response JSON into a Python dictionary
         data = response.json()
         # Do something with the data
-        dat = data['hourly']
+        dat = data['list']
         
         # Create an empty list to store DataFrames from each dictionary
         data_frames = []
@@ -74,6 +74,15 @@ def api_call_Open_weather_Map(url) -> pd.DataFrame:
         # Loop over the 
         for iS in dat:
             # 1. Extract and Flatten Weather Data
+            wind_info = iS.pop('wind')
+            iS.update(wind_info)
+
+            main_info = iS.pop('main')
+            iS.update(main_info)
+
+            cloud_info = iS.pop('clouds')
+            iS.update(cloud_info)
+
             weather_info = iS.pop('weather')[0]
             iS.update(weather_info)
 
@@ -83,10 +92,10 @@ def api_call_Open_weather_Map(url) -> pd.DataFrame:
         # Concatenate the DataFrames vertically (axis=0)
         df = pd.concat(data_frames, ignore_index=True)
         return df
-        # return thund
+        # return dat
 
     else:
         print(f'Failed to retrieve data: {response.status_code}')
 
 
-# print(api_call_Open_weather_Map(cities_url['Srinagar_URL']))
+# print(api_call_Open_weather_Map(cities_url['Bhopal_URL']))
